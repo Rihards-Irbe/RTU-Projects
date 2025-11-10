7facca064f02739fd2a094d193a0d605817b076f
Rihards <Rihards.Irbe@outlook.com>
2025-11-10 22:05:52 +0000

    Add files via upload

---
+++ MATLAB-projects/Skaitliskas-Metodes (Nummeric-Methods)/Praktiskie-Darbi (practical assignments)/2kd/uzdevums_8.m
@@ -0,0 +1,49 @@
+clc, clearvars, close all, format short
+
+xpoints = [1.0 1.5 2.0 2.5 3.0 3.5 4.0 4.5 5.0]';
+ypoints = [5.5 4.7 4.9 6.4 8.1 9.3 11.4 13.5 16.0]';
+
+figure
+plot(xpoints, ypoints, 'or', 'LineWidth', 3)
+grid on, title('Datu punkti')
+
+max_karta = 3;
+adj  = zeros(1, max_karta);
+rmse = zeros(1, max_karta);
+
+for n = 1:max_karta
+    poly_value = sprintf('poly%d', n);
+    [p, reg] = fit(xpoints, ypoints, poly_value)
+
+    p = polyfit(xpoints, ypoints, n);
+    xx = linspace(min(xpoints), max(xpoints), 1000);
+    yapprox = polyval(p, xx);
+
+    figure
+    plot(xpoints, ypoints, 'or', xx, yapprox, 'k', 'LineWidth', 3)
+    legend('data', 'fitted curve'), grid on
+    title(sprintf('Aproksimācija ar %d kārtas polinomu', n))
+
+    fun_prob1(p);
+
+    adj(n)  = reg.adjrsquare;
+    rmse(n) = reg.rmse;
+end
+
+
+[~, order] = sortrows([-adj(:), rmse(:)]);
+best_n = order(1);
+
+fprintf('\n Vislabākā aproksimācija ir %d. kārtas polinoms ',best_n)
+
+function fun_prob1(koef)
+ m = length(koef); n = m-1;
+ fprintf('\n Atbilde. %.0f. kārtas polinoms: \n ', n)
+ for i = 1:m
+   if koef(i) < 0, fprintf(' %.4fx^%.0f', koef(i), n);
+   else,            fprintf(' +%.4fx^%.0f', koef(i), n);
+   end
+   n = n-1;
+ end
+ fprintf('\n')
+end
